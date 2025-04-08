76c61581f3add936d8170d937a1a3f9cc132eebe
kangqiwang <kangqiwang@outlook.com>
2025-04-08 13:44:30 +0100

    update mrt

--- astropy/cosmology/_src/io/builtin/mrt.py
+++ astropy/cosmology/_src/io/builtin/mrt.py
@@ -7,9 +7,8 @@ The functions are registered with ``readwrite_registry`` under the format name "
 
 from __future__ import annotations
 
+import json
 from typing import TYPE_CHECKING, Any, TypeVar
-
-import numpy as np
 
 import astropy.cosmology.units as cu
 import astropy.units as u
@@ -17,33 +16,15 @@ from astropy.cosmology._src.core import Cosmology
 from astropy.cosmology._src.io.connect import readwrite_registry
 from astropy.io.typing import PathLike, ReadableFileLike, WriteableFileLike
 from astropy.table import QTable
-from astropy.table.serialize import represent_mixins_as_columns
 
 from .table import from_table, to_table
 
 if TYPE_CHECKING:
-    from collections.abc import Mapping
-
     from astropy.cosmology._src.typing import _CosmoT
     from astropy.io.typing import PathLike, ReadableFileLike, WriteableFileLike
     from astropy.table import Table
 
     _TableT = TypeVar("_TableT", "Table")
-
-_FORMAT_TABLE = {
-    "H0": "$$H_{0}$$",
-    "Om0": "$$\\Omega_{m,0}$$",
-    "Ode0": "$$\\Omega_{\\Lambda,0}$$",
-    "Tcmb0": "$$T_{0}$$",
-    "Neff": "$$N_{eff}$$",
-    "m_nu": "$$m_{nu}$$",
-    "Ob0": "$$\\Omega_{b,0}$$",
-    "w0": "$$w_{0}$$",
-    "wa": "$$w_{a}$$",
-    "wz": "$$w_{z}$$",
-    "wp": "$$w_{p}$$",
-    "zp": "$$z_{p}$$",
-}
 
 
 def read_mrt(
@@ -52,13 +33,30 @@ def read_mrt(
     *,
     move_to_meta: bool = False,
     cosmology: str | type[_CosmoT] | None = None,
-    rename: Mapping[str, str] | None = None,
     **kwargs: Any,
 ) -> _CosmoT:
     r"""Read a `~astropy.cosmology.Cosmology` from an MRT file.
 
     Parameters
     ----------
+
+    Examples
+    --------
+    We assume the following setup:
+
+        >>> from pathlib import Path
+        >>> from tempfile import TemporaryDirectory
+        >>> temp_dir = TemporaryDirectory()
+
+    Writing a cosmology to a LaTeX file will produce a table with the cosmology's type,
+    name, and parameters as columns.
+
+        >>> from astropy.cosmology import Planck18
+        >>> file = Path(temp_dir.name) / "file.mrt"
+
+        >>> Planck18.write(file, format="ascii.mrt")
+        >>> with open(file) as f: print(f.read())
+
 
 
 
@@ -67,27 +65,28 @@ def read_mrt(
     if format != "ascii.mrt":
         raise ValueError(f"format must be 'ascii.mrt',not {format}")
 
-    table = QTable.read(file, format="ascii.mrt", **kwargs)
+    with u.add_enabled_units(cu):
+        table = QTable.read(file, format="ascii.mrt", **kwargs)
 
-    m_nu_data = []
-    i, more_m_nu = 0, True
-    while more_m_nu:
-        cn = f"m_nu[{i}]"  # column name
-        if cn not in table.colnames:
-            more_m_nu = False
-            continue
+    # m_nu_data = []
+    # i, more_m_nu = 0, True
+    # while more_m_nu:
+    #     cn = f"m_nu[{i}]"  # column name
+    #     if cn not in table.colnames:
+    #         more_m_nu = False
+    #         continue
 
-        m_nu_data.append(table[cn][0])
-        table.remove_column(cn)
+    #     m_nu_data.append(table[cn][0])
+    #     table.remove_column(cn)
 
-        i += 1
+    #     i += 1
 
-    col = (
-        [m_nu_data]
-        if not isinstance(m_nu_data[0], u.Quantity)
-        else [u.Quantity(m_nu_data)]
-    )
-    table.add_column(col, name="m_nu", index=-2)
+    # col = (
+    #     [m_nu_data]
+    #     if not isinstance(m_nu_data[0], u.Quantity)
+    #     else [u.Quantity(m_nu_data)]
+    # )
+    # table.add_column(col, name="m_nu", index=-2)
 
     # Build the cosmology from table, using the private backend.
     return from_table(
@@ -102,38 +101,61 @@ def write_mrt(
     overwrite: bool = False,
     cls: type[_TableT] = QTable,
     latex_names: bool = False,
+    cosmology_in_meta: bool = True,
     **kwargs: Any,
 ):
     format = kwargs.pop("format", "ascii.mrt")
     if format != "ascii.mrt":
         raise ValueError(f"format must be 'ascii.mrt', not {format}")
 
-    table = represent_mixins_as_columns(
-        to_table(cosmology, cls=cls, cosmology_in_meta=False)
-    )
-
-    # CDS can't serialize redshift units, so remove them  # TODO: fix this
+    table = to_table(cosmology, cls=cls, cosmology_in_meta=cosmology_in_meta)
     for k, col in table.columns.items():
+        # CDS can't serialize redshift units, so remove them  # TODO: fix this
         if col.unit is cu.redshift:
             table[k] <<= u.dimensionless_unscaled
+        ## check if col is mutil den
+        if len(col.shape) > 1 or col.info.dtype.kind == "0":
 
-    # Replace the m_nu column with individual columns
-    if "m_nu" in table.colnames:
-        m_nu = table["m_nu"]
-
-        index = table.colnames.index("m_nu")
-        table.remove_column("m_nu")
-        m_nu = np.atleast_1d(m_nu)  # Ensure m_nu is an array
-        if m_nu.shape == (1,):
-            # If m_nu is a scalar, add a single column
-            table.add_column(m_nu[0], name="m_nu[0]", index=index)
+            def format_col_item(idx):
+                obj = col[idx]
+                try:
+                    obj = obj.value.tolist()
+                except AttributeError:
+                    pass
+                return json.dumps(obj, separators=(",", ":"))
         else:
-            # If m_nu is an array, add multiple columns with names 'm_nu_[i]'
-            cols, names = tuple(
-                zip(*((m, f"m_nu[{i}]") for i, m in enumerate(m_nu[0])))
-            )
-            table.add_columns(cols, names=names, indexes=(index,) * len(m_nu[0]))
 
+            def format_col_item(idx):
+                return str(col[idx])
+
+        try:
+            table[k] = [format_col_item(idx) for idx in range(len(col))]
+
+        except TypeError as exc:
+            raise TypeError(
+                f"could not convert column {col.info.name!r} to string: {exc}"
+            ) from exc
+
+    # remove multidimensional columns
+
+    # # Replace the m_nu column with individual columns
+    # if "m_nu" in table.colnames:
+    #     m_nu = table["m_nu"]
+
+    #     index = table.colnames.index("m_nu")
+    #     table.remove_column("m_nu")
+    #     m_nu = np.atleast_1d(m_nu)  # Ensure m_nu is an array
+    #     if m_nu.shape == (1,):
+    #         # If m_nu is a scalar, add a single column
+    #         table.add_column(m_nu[0], name="m_nu[0]", index=index)
+    #     else:
+    #         # If m_nu is an array, add multiple columns with names 'm_nu_[i]'
+    #         cols, names = tuple(
+    #             zip(*((m, f"m_nu[{i}]") for i, m in enumerate(m_nu[0])))
+    #         )
+    #         table.add_columns(cols, names=names, indexes=(index,) * len(m_nu[0]))
+
+    # Write MRT
     table.write(file, overwrite=overwrite, format="ascii.mrt", **kwargs)
 
 
